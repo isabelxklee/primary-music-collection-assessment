edfc7d1ab96c5bdd517dafac480da000e4d745ae
isabelxklee <hi@kleetime.com>
2022-10-22 15:14:47 -0400

    added show all command

---
+++ bin/show-all.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import { albums } from "../variables.js";
+import { runCommands } from "./logger.js";
+
+export const showAll = () => {
+  let results = albums.map((album) => {
+    console.log(
+      `"${album.title}" by ${album.artist} (${
+        album.played ? "played" : "unplayed"
+      })`
+    );
+  });
+
+  albums.length > 0
+    ? results
+    : console.log(
+        "Oops! There aren't any albums yet. Try adding a new album with the `add <album> <artist>` command."
+      );
+};
+
+runCommands();
