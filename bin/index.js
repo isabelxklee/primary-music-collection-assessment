12d52c9dd21fb98a0c8f65c5452716e5d8504811
isabelxklee <hi@kleetime.com>
2022-10-22 15:08:08 -0400

    updated welcome message

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log("hello world");
+const welcomeMessage = `
+  🎵 Welcome to your music collection!
+  🎵
+  This app helps you keep track of the albums you've added and played.
+
+  commands:
+    music                       start the app
+    add <album-title> <artist>  add a new album to your list (unplayed by default)
+    play <album-title>          marks an album as "played"
+    show all                    displays all the albums in the collection
+    show all by <artist>        display all the albums by a given artist
+    show unplayed               display all the unplayed albums in the collection
+    show unplayed by <artist>   display all the unplayed albums by a given artist
+    quit                        closes the program and your session
+`;
+
+console.log(welcomeMessage);
