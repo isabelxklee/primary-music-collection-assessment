4b0272712201d5c9528c40d87f5c391e40051386
isabelxklee <hi@kleetime.com>
2022-10-22 15:21:38 -0400

    created add command

---
+++ bin/add.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import { albums } from "../variables.js";
+import { runCommands } from "./logger.js";
+
+export const addAlbum = (answer) => {
+  console.log(answer);
+
+  // let results = albums.filter((album) => {
+  //   return album.title.toLowerCase() === albumTitle.toLowerCase();
+  // });
+
+  // // if the results array's length is longer than 0
+  // // that means that we have a duplicate
+  // // display error message to user
+  // if (results.length > 0) {
+  //   console.log(
+  //     "Error: this album title already exists in the database. Please try adding a different album!"
+  //   );
+  // } else {
+  //   // otherwise, push the album data to the db
+  //   albums.push({
+  //     title: albumTitle,
+  //     artist: artist,
+  //     played: false,
+  //   });
+  //   db.write();
+
+  //   // display added album back to user
+  //   console.log(`Added "${albumTitle}" by ${artist}`);
+  // }
+};
