927116430e63b4d357e33e1ea64080853705925a
isabelxklee <hi@kleetime.com>
2022-10-24 00:12:28 -0400

    updated play tests

--- tests/play.test.js
+++ tests/play.test.js
@@ -6,21 +6,15 @@ describe("play an album", () => {
   test("that exists and hasn't been played", () => {
     console.log = jest.fn();
     addAlbum(`add "The Dark Side of the Moon" "Pink Floyd"`);
-    const data = playAlbum("The Dark Side of the Moon");
+    const data = playAlbum(`play "The Dark Side of the Moon"`);
     expect(data).toBe(`You're listening to "The Dark Side of the Moon"`);
-    expect(console.log.mock.calls[0][0]).toBe(
-      `You're listening to "The Dark Side of the Moon"`
-    );
   });
 
   test("that exists and has already been played", () => {
     console.log = jest.fn();
     addAlbum(`add "Ride the Lightning" "Metallica"`);
-    playAlbum("Ride the Lightning");
-    const data = playAlbum("Ride the Lightning");
+    playAlbum(`play "Ride the Lightning"`);
+    const data = playAlbum(`play "Ride the Lightning"`);
     expect(data).toBe("This album has already been played.");
-    expect(console.log.mock.calls[0][0]).toBe(
-      "This album has already been played."
-    );
   });
 });
