9deb9e70ad3584b6b5c275059604a96bdd664cad
isabelxklee <hi@kleetime.com>
2022-10-24 00:14:17 -0400

    added test to play command

--- tests/play.test.js
+++ tests/play.test.js
@@ -1,20 +1,22 @@
 import { playAlbum } from "../bin/play.js";
 import { addAlbum } from "../bin/add.js";
-import { jest } from "@jest/globals";
 
 describe("play an album", () => {
   test("that exists and hasn't been played", () => {
-    console.log = jest.fn();
     addAlbum(`add "The Dark Side of the Moon" "Pink Floyd"`);
     const data = playAlbum(`play "The Dark Side of the Moon"`);
     expect(data).toBe(`You're listening to "The Dark Side of the Moon"`);
   });
 
   test("that exists and has already been played", () => {
-    console.log = jest.fn();
     addAlbum(`add "Ride the Lightning" "Metallica"`);
     playAlbum(`play "Ride the Lightning"`);
     const data = playAlbum(`play "Ride the Lightning"`);
     expect(data).toBe("This album has already been played.");
+  });
+
+  test("that doesn't exist", () => {
+    const data = playAlbum(`play "Pauls Boutique"`);
+    expect(data).toBe("Oops! That album doesn't exist.");
   });
 });
