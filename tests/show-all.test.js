997b71b8d1453429733bc23dc50f6e4f181aadb7
isabelxklee <hi@kleetime.com>
2022-10-24 00:17:56 -0400

    updated tests for show all

--- tests/show-all.test.js
+++ tests/show-all.test.js
@@ -1,7 +1,17 @@
 import { showAll } from "../bin/show-all.js";
-import { toBeArray } from "jest-extended";
+import { addAlbum } from "../bin/add.js";
+import { toBeArrayOfSize } from "jest-extended";
 
-test("shows all existing albums", () => {
-  const data = showAll();
-  expect(data).toBeArray();
+describe("play all albums", () => {
+  test("that don't exist", () => {
+    const data = showAll();
+    expect(data).toBeArrayOfSize(0);
+  });
+
+  test("that exist", () => {
+    addAlbum(`add "The Dark Side of the Moon" "Pink Floyd"`);
+    addAlbum(`add "Ride the Lightning" "Metallica"`);
+    const data = showAll();
+    expect(data).toBeArrayOfSize(2);
+  });
 });
